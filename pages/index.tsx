9e85587645088d0e59a70d374f52771cae765513
MadMan24 <mdverdera@yahoo.com>
2022-10-03 15:52:10 +0800

    Initial commit for setup and configuration.

---
+++ pages/index.tsx
@@ -0,0 +1,17 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
+
+const Home: NextPage = () => {
+  return (
+    <>
+      <Head>
+        <title>Bookmark Website</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    </>
+  );
+};
+
+export default Home;
