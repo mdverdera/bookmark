8fcee6b90e3cfd56469bb5eb45643fff8d61d242
MadMan24 <mdverdera@yahoo.com>
2022-10-03 16:06:13 +0800

    Navigation menu.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
+import Link from "next/link";
 
 const Home: NextPage = () => {
   return (
@@ -9,7 +10,36 @@ const Home: NextPage = () => {
         <title>Bookmark Website</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <nav className="container relative mx-auto p-6">
+        {/* Flex container for nav items */}
+        <div className="flex items-center justify-between space-x-20 my-6">
+          {/* Logo */}
+          <div className="z-30">
+            <picture>
+              <img src="/img/logo-bookmark.svg" alt="logo" />
+            </picture>
+          </div>
+
+          {/* Menu Items */}
+          <div className="hidden items-center space-x-10 uppercase text-grayishBlue md:flex">
+            <Link href="#features" scroll={false}>
+              <a className="tracking-widest hover:text-softRed">Features</a>
+            </Link>
+            <Link href="#download" scroll={false}>
+              <a className="tracking-widest hover:text-softRed">Download</a>
+            </Link>
+            <Link href="#faq" scroll={false}>
+              <a className="tracking-widest hover:text-softRed">FAQ</a>
+            </Link>
+
+            <Link href="#">
+              <a className="px-8 py-2 text-white bg-softRed border-2 border-softRed rounded-lg shadow-md hover:text-softRed hover:bg-white">
+                Login
+              </a>
+            </Link>
+          </div>
+        </div>
+      </nav>
     </>
   );
 };
